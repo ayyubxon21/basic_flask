cf60fb95f51cbe16dd3fa21b220f60bd1f02e17d
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2023-02-10 07:46:20 -0800

    evrika

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1> Hello, World!</h1>'
+    return '<h1> Hello, github!</h1>'
 @app.route('/indes')
 @app.route('/home')
 def home():
